628d68a78cd149bdb8163c44f3667ecdaf4d678d
hungtcs <hungtcs@outlook.com>
2022-11-17 16:43:40 +0800

    feat: add commonjs support

---
+++ rollup.config.ts
@@ -0,0 +1,42 @@
+import rollupPluginDelete from 'rollup-plugin-delete';
+import rollupPluginTypescript from '@rollup/plugin-typescript';
+import { RollupOptions } from 'rollup';
+import { terser as rollupPluginTerser } from 'rollup-plugin-terser';
+
+const production = process.env.NODE_ENV === 'production';
+
+const options: RollupOptions = {
+  input: './lib/index.ts',
+  output: [
+    {
+      dir: './dist/',
+      format: 'esm',
+      sourcemap: !production,
+      entryFileNames: 'index.mjs',
+    },
+    {
+      dir: './dist/',
+      name: 'UrlComponentJoin',
+      format: 'umd',
+      sourcemap: !production,
+      entryFileNames: 'index.js',
+    },
+  ],
+  plugins: [
+    rollupPluginDelete(
+      {
+        targets: [
+          './dist/**/*'
+        ],
+      }
+    ),
+    rollupPluginTypescript(
+      {
+        tsconfig: 'tsconfig.lib.json'
+      },
+    ),
+    production && rollupPluginTerser(),
+  ],
+};
+
+export default options;
